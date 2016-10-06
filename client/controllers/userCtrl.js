c26ccef1cc87cf85204ae876f1b5eb1b4a0b8b3d
= <ysanchez3d@gmail.com>
2016-10-05 21:19:18 -0700

    prepping the controller to extract character information to my needs

--- client/controllers/userCtrl.js
+++ client/controllers/userCtrl.js
@@ -1,7 +1,58 @@
 'use strict';
 
 angular.module('easyRaidFinder')
-.controller('userCtrl', function($http, $location) {
+.controller('userCtrl', function($scope, $http, $location) {
+
+  var accessToken = localStorage.getItem("access_token");
+
+  if (accessToken === null) {
+    $location.path('/');
+  }
+
+  var charactersApi = 'https://us.api.battle.net/wow/user/characters'
+
+  $http.get(charactersApi + "?access_token=" + accessToken).then(function(res) {
+    $scope.processCharacters(res.data.characters);
+  })
+
+  $scope.realmInfo;
+
+
+  $scope.processCharacters = function(chars) {
+
+    var realms = {};
+    var classes = {0: 'none', 1: 'Warrior', 2: 'Paladin', 3: 'Hunter', 4: 'Rogue', 5: 'Priest',
+                   6:'Death Knight', 7:'Shaman', 8:'Mage', 9:'Warlock', 10:'Monk', 11:'Druid', 12:'Demon Hunter'};
+
+    var races = {1: 'Human', 2: 'Orc', 3: 'Dwarf', 4: 'Night Elf', 5: 'Undead', 6: 'Tauren', 7: 'Gnome', 8: 'Troll',
+                 9: 'Goblin', 10: 'Blood Elf', 11: 'Draenei', 22: 'Worgen', 24: 'Panda', 25: 'Panda', 26: 'Panda'  };
+    var genders = {0: 'Male', 1: 'Female'};
+
+    chars.forEach(function(char) {
+      var charRealm = char.realm;
+
+      var charInfo = {
+        name: char.name,
+        level: char.level,
+        class: classes[char.class],
+        race: races[char.race],
+        gender: genders[char.gender]
+      }
+
+      if (realms[charRealm] === undefined) {
+        realms[charRealm] = [];
+      }
+
+      realms[charRealm].push(charInfo);
+    })  
+
+
+
+    $scope.realmInfo = realms;
+    console.log($scope.realmInfo);
+  }
+
+  console.log($scope.realmInfo);
 
 
 });
